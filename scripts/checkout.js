7091b9793dcb75a24f18e9b177e57d979fa5a169
ashutoshdash9878 <101615795+ashutoshdash9878@users.noreply.github.com>
2022-04-25 13:06:57 +0530

    Update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,29 @@
 // Each ticket will cost 100 Rupees
 // If wallet amount is insufficient show alert "Insufficient Balance!";
 // Else show "Booking successfull!" and adjust the wallet amount in real time
+
+
+var amount=document.getElementById("wallet")
+    amount.innerText=JSON.parse(localStorage.getItem("amount"))
+
+    var movi=JSON.parse(localStorage.getItem("movie")) || [];
+    console.log(movi);
+
+
+
+
+    movi.map(function(element){
+      var div=document.createElement("div")
+
+
+      var image=document.createElement("img")
+        image.src=element.Poster
+
+      var title=document.createElement("h2")
+      title.innerText=element.Title
+    
+    div.append(title,image)
+
+    document.getElementById("movie").append(div)
+    
+    })
